fd18537068b85e019ab15ae6f55e8db3e96b6e08
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-09-16 17:30:34 +0300

    done

--- public/index.js
+++ public/index.js
@@ -1,36 +1,39 @@
-const overlay = document.querySelector(".back")
-const bookingForm = document.querySelector("section.booking_form .contain")
+const overlay = document.querySelector(".back");
+const bookingForm = document.querySelector("section.booking_form .contain");
+const loading = document.querySelector(".loading");
 
 //Buttons
-const addButton = document.getElementById("add_button")
-const closeButton = document.getElementById("close_button")
-const cancelButton = document.getElementById("cancel_button")
-const saveButton = document.getElementById("save_button")
+const addButton = document.getElementById("add_button");
+const closeButton = document.getElementById("close_button");
+const cancelButton = document.getElementById("cancel_button");
+const saveButton = document.getElementById("save_button");
 
 //Select & inputs
-const time = document.getElementById("time")
-const closerName = document.getElementById("closer_name")
-const customerName = document.getElementById("customer_name")
-const customerEmail = document.getElementById("customer_email")
-const hotelName = document.getElementById("hotel_name")
-const hotelPrice = document.getElementById("hotel_price")
+const time = document.getElementById("time");
+const closerName = document.getElementById("closer_name");
+const customerName = document.getElementById("customer_name");
+const customerEmail = document.getElementById("customer_email");
+const hotelName = document.getElementById("hotel_name");
+const hotelPrice = document.getElementById("hotel_price");
 
-function close_form (){
-    overlay.classList.remove("overlay")
-    bookingForm.classList.remove("active")
+function close_form() {
+  overlay.classList.remove("overlay");
+  bookingForm.classList.remove("active");
+  document.body.classList.remove("active");
 }
 
-addButton.addEventListener("click",()=>{
-    overlay.classList.add("overlay")
-    bookingForm.classList.add("active")
-})
+addButton.addEventListener("click", () => {
+  overlay.classList.add("overlay");
+  bookingForm.classList.add("active");
+  document.body.classList.add("active");
+});
 
-overlay.addEventListener("click",close_form)
+overlay.addEventListener("click", close_form);
 
-closeButton.addEventListener("click",close_form)
+closeButton.addEventListener("click", close_form);
 
-cancelButton.addEventListener("click",close_form)
+cancelButton.addEventListener("click", close_form);
 
-saveButton.addEventListener("click",()=>{
-    
-})
+saveButton.addEventListener("click", () => {
+  loading.classList.add("active");
+});
