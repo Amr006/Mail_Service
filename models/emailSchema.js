bc7be568de291a48d4915eb3099197671a663d68
Amr006 <2021170374@cis.asu.edu.eg>
2023-09-19 20:39:45 +0300

    fixing time zone

--- models/emailSchema.js
+++ models/emailSchema.js
@@ -27,4 +27,11 @@ const emailSchema = Schema({
 },{
   timestamps: true, // Enable timestamps
 })
+
+emailSchema.pre('save', function (next) {
+  // Adjust the createdAt field with your desired timezone offset (e.g., +3 hours for Egypt)
+  this.createdAt = new Date(Date.now() + 1 * 60 * 60 * 1000);
+  next();
+});
+
 module.exports = mongoose.model("Email" , emailSchema)
