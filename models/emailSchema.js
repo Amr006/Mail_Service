4901c7c4bdc06b78784808a2ea7a80fe7999c7a7
Amr006 <2021170374@cis.asu.edu.eg>
2023-09-19 21:05:14 +0300

    fixing time zone

--- models/emailSchema.js
+++ models/emailSchema.js
@@ -30,7 +30,7 @@ const emailSchema = Schema({
 
 emailSchema.pre('save', function (next) {
   // Adjust the createdAt field with your desired timezone offset (e.g., +3 hours for Egypt)
-  this.createdAt = new Date(Date.now() + 1 * 60 * 60 * 1000);
+  this.createdAt = new Date(Date.now() + 3 * 60 * 60 * 1000);
   next();
 });
 
